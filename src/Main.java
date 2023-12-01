1ec28a9ceada304be569129e984049cde4b56b70
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-11-30 19:38:14 -0700

    Gottafigure out try and catch

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        double subtotal = 0;
 
         File file = new File( "\"C:\\Users\\aliba\\Documents\\Class\\TotalCalculation\\src\\items.txt\"");
-        try {
+        try  {
             Scanner sc = new Scanner(file);
         } catch (IOException e){
 
         }
         ArrayList<Double> reader = new ArrayList<Double>();
-        while (itr.hasNextDouble())
-            System.out.println(itr.next());
-    }
+
+        while (sc.hasNextLine) {
+            subtotal = sc.hasNextDouble();
+            subtotal = subtotal + subtotal;
+        }
+        double tax = subtotal*.053;
+        double total = subtotal+tax;
 }
